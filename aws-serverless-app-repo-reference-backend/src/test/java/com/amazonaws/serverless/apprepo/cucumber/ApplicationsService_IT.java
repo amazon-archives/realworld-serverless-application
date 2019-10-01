61b8079695996380e5cb8272959abd056acac874
Lu Hong <luhong@amazon.com>
2019-10-01 09:45:16 -0700

    fix cucumber tests path

--- aws-serverless-app-repo-reference-backend/src/test/java/com/amazonaws/serverless/apprepo/cucumber/ApplicationsService_IT.java
+++ aws-serverless-app-repo-reference-backend/src/test/java/com/amazonaws/serverless/apprepo/cucumber/ApplicationsService_IT.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
  * Junit entry point for cucumber tests.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/com/luhong/apprepo/cucumber/features",
+@CucumberOptions(features = "src/test/java/com/amazonaws/serverless/apprepo/cucumber/features",
       glue = "com.amazonaws.serverless.apprepo.cucumber.steps",
       tags = {"not @NotImplemented"},// Use @NotImplemented tag for scenarios that have not been implemented
       plugin = {"pretty"})
