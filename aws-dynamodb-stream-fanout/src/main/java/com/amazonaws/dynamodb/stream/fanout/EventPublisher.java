ab104f91edce08f59b8ea3a9e27b9c003acf87f8
Lu Hong <luhong@amazon.com>
2019-10-02 21:34:51 -0700

    chore: move fanout module to its own repo

--- aws-dynamodb-stream-fanout/src/main/java/com/amazonaws/dynamodb/stream/fanout/EventPublisher.java
+++
@@ -1,7 +0,0 @@
-package com.amazonaws.dynamodb.stream.fanout;
-
-import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
-
-public interface EventPublisher {
-    void publish(DynamodbEvent event);
-}
