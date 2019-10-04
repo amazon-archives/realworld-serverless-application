6cd32f434ddf5be88eca968a7b05628d494dc678
Lu Hong <luhong@amazon.com>
2019-10-03 22:18:05 -0700

    feat: add template for the whole project

--- bin/package-all.sh
+++ bin/package-all.sh
@@ -11,9 +11,5 @@
 set -e # fail script on any individual command failing
 
 DIR=`dirname "${BASH_SOURCE[0]}"`
-MODULES=(aws-serverless-app-repo-reference-backend aws-serverless-app-repo-reference-ops aws-serverless-app-repo-reference-static-website)
 echo "Packaging all modules..."
-for MODULE in ${MODULES[@]}
-    do
-        ${DIR}/package.sh -n ${MODULE}
-    done
+${DIR}/package.sh -n .
