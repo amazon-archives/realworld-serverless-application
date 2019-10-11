d492dba22b0676529c037fd723d50666ba4bf2cd
Miles <milelaff@amazon.com>
2019-10-11 12:42:55 -0700

    Updated cucumber feature file directory

--- backend/src/test/java/software/amazon/serverless/apprepo/cucumber/ApplicationsService_IT.java
+++ backend/src/test/java/software/amazon/serverless/apprepo/cucumber/ApplicationsService_IT.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
  * Junit entry point for cucumber tests.
  */
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/com/amazonaws/serverless/apprepo/cucumber/features",
+@CucumberOptions(features = "src/test/java/software/amazon/serverless/apprepo/cucumber/features",
       glue = "software.amazon.serverless.apprepo.cucumber.steps",
       tags = {"not @NotImplemented"},// Use @NotImplemented tag for scenarios that have not been implemented
       plugin = {"pretty"})
