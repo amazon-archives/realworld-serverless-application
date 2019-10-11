303c2d4a1efc0d75bba2c5e854cfa081a365132a
Lu Hong <luhong07@gmail.com>
2019-10-10 19:33:17 -0700

    Update static-website/src/views/Home.vue
    Co-Authored-By: Nestor Carvantes <carvantes@gmail.com>

--- static-website/src/views/Home.vue
+++ static-website/src/views/Home.vue
@@ -5,7 +5,7 @@
   >
 
     <hr class="my-4" />
-    <p>This is a cover page for the front end module of the awslab/realworld-serverless-application Project. See more details <a href="https://github.com/awslabs/realworld-serverless-application/wiki">here</a>.</p>
+    <p>This is a cover page for the front end module of the awslabs/realworld-serverless-application project. See more details <a href="https://github.com/awslabs/realworld-serverless-application/wiki">here</a>.</p>
 
     <b-button id="try-demo" variant="primary" to="/applications">Try the Demo</b-button>
     &nbsp;
